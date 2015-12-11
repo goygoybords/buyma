eaf12d61c588307cf9f602e6e9a7a7c01bc36b18
goygoybords <kevinseankho@yahoo.com>
2015-12-11 15:21:09 +0800

    branch commit

--- wp-content/themes/twentyfifteen/woocommerce/single-product.php
+++ wp-content/themes/twentyfifteen/woocommerce/single-product.php
@@ -14,15 +14,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 get_header(); 
-	 do_action( 'woocommerce_before_single_product' );
+	 
 
 	 if ( post_password_required() ) {
 	 	echo get_the_password_form();
 	 	return;
 	 }
+
+
+	while ( have_posts() ) : the_post(); 
+
+
+
 ?>
 	<div class="bmg-l-wrapper js-bmg-l-wrapper" >
 			<div class="bmg-l-breadcrumbs">
+				<?php do_action( 'woocommerce_before_single_product' ); ?>
 				<?php echo woocommerce_breadcrumb(); ?>
 			</div>
 			<div class="bmg-l-layout--full">
@@ -31,98 +38,58 @@ get_header();
 						<div class="item-images-wrap">
 							<div class="bx-wrapper" style="max-width: 100%;">
 								<div class="bx-viewport" aria-live="polite" style="width: 100%; overflow: hidden; position: relative; height: 500px;">
-									<ul class="js-item-images" style="width: 4215%; position: relative; transition-duration: 0.5s; transform: translate3d(-1500px, 0px, 0px);">
-										<li style="float: left; list-style: outside none none; position: relative; width: 500px;" aria-hidden="true">
+									<ul class="js-item-images" >
+										<li>
 											<div class="item-image">
-												<img src="https://s3.amazonaws.com/image.buyma.com/imgdata/item/151113/0018627881/org_1.jpg">
-											</div>
-										</li>
-										<li style="float: left; list-style: outside none none; position: relative; width: 500px;" aria-hidden="true">
-											<div class="item-image">
-												<img src="https://s3.amazonaws.com/image.buyma.com/imgdata/item/151113/0018627881/org_2.jpg">
-											</div>
-										</li>
-										<li style="float: left; list-style: outside none none; position: relative; width: 500px;" aria-hidden="true">
-											<div class="item-image">
-												<img src="https://s3.amazonaws.com/image.buyma.com/imgdata/item/151113/0018627881/org_3.jpg">
-											</div>
-										</li>
-										<li style="float: left; list-style: outside none none; position: relative; width: 500px;" aria-hidden="false">
-											<div class="item-image">
-												<img src="https://s3.amazonaws.com/image.buyma.com/imgdata/item/151113/0018627881/org_4.jpg">
+												<?php woocommerce_show_product_images(); ?>
 											</div>
 										</li>
 									</ul>
 								</div>
-								<div class="bx-controls bx-has-controls-direction">
-									<div class="bx-controls-direction">
-										<a href="" class="bx-prev"><i class="bmg-c-icon--arrow-left"></i></a>
-										<a href="" class="bx-next disabled"><i class="bmg-c-icon--arrow-right"></i></a>
-									</div>
-								</div>
-							</div>
-							<div class="js-bx-pager item-pager">
-								<a href="javascript:void(0);" data-slide-index="0" class="js-thumb-image">
-									<img src="https://s3.amazonaws.com/image.buyma.com/imgdata/item/151113/0018627881/org_1.jpg"></a>
-									<a href="javascript:void(0);" data-slide-index="1" class="js-thumb-image">
-										<img src="https://s3.amazonaws.com/image.buyma.com/imgdata/item/151113/0018627881/org_2.jpg"></a>
-										<a href="javascript:void(0);" data-slide-index="2" class="js-thumb-image">
-											<img src="https://s3.amazonaws.com/image.buyma.com/imgdata/item/151113/0018627881/org_3.jpg"></a>
-											<a href="javascript:void(0);" data-slide-index="3" class="js-thumb-image active">
-												<img src="https://s3.amazonaws.com/image.buyma.com/imgdata/item/151113/0018627881/org_4.jpg"></a>
-												<a href="javascript:void(0);" class="js-pager-current" style="left: 155px; top: 0px;"></a>
 							</div>
 						</div>
 					</div>
 					<div class="item-section--r">
 						<div class="item-detail">
 							<div class="item-detail__section">
-								<h1 class="item-ttl">ASOS [More Hair Accessories] 1c/1s</h1>
+								<h1 class="item-ttl"><?php woocommerce_template_single_title(); ?></h1>
 								<h2 class="item-subttl--brand">
-									<a href="/ph/items/find/asos/">ASOS</a>
+
+									<?php echo $product->get_attribute( 'Brand' ); ?>
 								</h2>
 							</div>
 							<div class="item-detail__section">
-								<div class="item-detail__userprice">¥6,700 JPY</div>
-							<div class="item-detail__price"><span>est.</span>$54.37 USD</div>
-							<h2 class="item-detail__subttl">Color</h2>
-							<span id="js-choose_color" class="error-msg" style="display: none;">Please Choose A Color</span>
-							<div class="item-one--color">
-								<input type="radio" name="product[color]" value="f9fb0fd3-7068-42cb-8977-0c3d205914ee" data-name="Multimedia" data-enable-size="7cc92ce8-7a7d-4ada-8ada-b9e68daecb3d" class="js_color" id="js_color_f9fb0fd3-7068-42cb-8977-0c3d205914ee">
-								<label title="" for="js_color_f9fb0fd3-7068-42cb-8977-0c3d205914ee" class="js_color_label  item__radio-label multi" data-original-title="Multimedia"></label>
+								<div class="item-detail__userprice">
+									<?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt )  ?>
+								</div> 
+							<div class="item-detail__price">
+								<?php woocommerce_template_single_price(); //get_woocommerce_currency_symbol().get_post_meta( get_the_ID(), '_regular_price', true); ?> 
 							</div>
-							<div class="color-title">Multimedia</div>
-							<h2 class="item-detail__subttl">Size</h2>
-							<span id="js-choose_size" class="error-msg" style="display: none;">Please Choose A Size</span>
-							<div class="item-one--size">
-								<input type="radio" name="product[size]" value="7cc92ce8-7a7d-4ada-8ada-b9e68daecb3d" data-name="ONE SIZE" data-enable-color="f9fb0fd3-7068-42cb-8977-0c3d205914ee" class="js_size" id="js_size_7cc92ce8-7a7d-4ada-8ada-b9e68daecb3d">
-								<label title="ONE SIZE" for="js_size_7cc92ce8-7a7d-4ada-8ada-b9e68daecb3d" class="item__radio-label"></label>
-							</div>
-							<div class="size-title">ONE SIZE</div>
+							
 							<div class="item-detail__add-btn">
-								
-								<a href="javascript:void(0);" data-item-id="75c90161-1328-4858-9e27-3a3e03a53ff8" class="bmg-btn--primary js-add-to-bag item-detail__btn--buyable">Add to Bag</a>
+								<?php 
+									//woocommerce_external_add_to_cart(); 
+									woocommerce_simple_add_to_cart(); 
+								?>
+								<!-- <a href="javascript:void(0);" data-item-id="75c90161-1328-4858-9e27-3a3e03a53ff8" 
+								class="bmg-btn--primary js-add-to-bag item-detail__btn--buyable">Add to Bag</a> -->
 							</div>
-							<p class="item-detail__mention-txt">*currency conversion is for reference only. Items will be charged in JPY.</p>
+							<!-- <p class="item-detail__mention-txt">
+								*currency conversion is for reference only. Items will be charged in JPY.
+							</p> -->
 						</div>
 						<div class="item-detail__section">
 							<div class="item-detail__descwrap">
 								<span class="descttl">Available Pieces</span>
-								<span class="descbody"> : 3</span>
+								<span class="descbody"> :
+									<?php echo $product->get_stock_quantity(); ?>
+								</span>
 							</div>
 							<div class="item-detail__descwrap">
 								<span class="descttl">Category</span>
 								<span class="descbody"> : 
-									<a href="/ph/items/find/asos/women/accessories/hair-accessories/more/">ASOS x More Hair Accessories</a></span>
-							</div>
-							<div class="item-detail__descwrap">
-								<span class="desc--ttl">Shipping From</span>
-								<span class="desc--body"> : 
-									<img width="18" alt="Jpn 4e636028ca" src="/assets/images/flag/jpn-4e636028ca.png"></span>
-							</div>
-							<div class="item-detail__descwrap">
-								<span class="desc--ttl">Shoppable Until</span>
-								<span class="desc--body"> : 2016-02-10 14:59:59 UTC</span>
+									<?php echo $product->get_categories(); ?>
+								</span>
 							</div>
 						</div>
 					</div>
@@ -131,16 +98,14 @@ get_header();
 			<div class="item-content--bottom">
 				<div class="item-section--l">
 					<div class="item-detail__section">
-						<h2 class="item-subttl">Comment</h2>
+						<h2 class="item-subttl">Item Details</h2>
 						<div class="item-detail__bodytxt">
-							<div class="js-detail-lead-first">Please enjoy shopping with buymore01!</div>
+							<?php 
+								the_content();
+							?>
 						</div>
 					</div>
-					<div class="item-detail__section">
-						<h2 class="item-subttl">Item Details</h2>
-						<div class="item-detail__bodytxt">If you have any questions about this item (ex. size, color, stock availability etc.), please click the 'Questions?' button and ask me!</div>
-					</div>
-					<div class="item-detail__section">
+					<!-- <div class="item-detail__section">
 						<h2 class="item-subttl">Shipping Method</h2>
 						<table class="item-shipping-table">
 							<thead>
@@ -158,7 +123,7 @@ get_header();
 								</tr>
 							</tbody>
 						</table>
-					</div>
+					</div> -->
 				</div>
 				<div class="item-section--r">
 					<div class="item-detail">
@@ -259,22 +224,23 @@ get_header();
 			<div class="footer-bnr--devide3">
 				<div class="footer-bnr--img">
 					<a href="/aboutbuyma/">
-						<img src="/assets/images/top/bnr_top_buyma01-06803a16b1.jpg" alt="Personal Service">
+						<img src="<?php bloginfo('template_url');?>/images/personal.jpg" alt="Personal Service">
 					</a>
 				</div>
 				<div class="footer-bnr--img">
 					<a href="/aboutbuyma/">
-						<img src="/assets/images/top/bnr_top_buyma03-4dc7b141a3.jpg" alt="Global Shopping with Local Prices">
+						<img src="<?php bloginfo('template_url');?>/images/global.jpg" alt="Global Shopping with Local Prices">
 					</a>
 				</div>
 				<div class="footer-bnr--img">
 					<a href="/aboutbuyma/">
-						<img src="/assets/images/top/bnr_top_buyma02-bcbf6120f4.jpg" alt="Style Inspirations ">
+						<img src="<?php bloginfo('template_url');?>/images/style.jpg" alt="Style Inspirations ">
 					</a>
 				</div>
 			</div>
 		</div>
 		<script type="application/json" id="js-item-data">{"id":"75c90161-1328-4858-9e27-3a3e03a53ff8","name":"ASOS [More Hair Accessories] 1c/1s","category":"Women/Accessories/Hair Accessories/More Hair Accessories","brand":"ASOS","price":6700.0}</script>
 	</div>
+	<?php endwhile; ?>
 
 <?php get_footer(); ?>
