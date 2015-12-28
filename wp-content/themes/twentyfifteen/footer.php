bae63c0d8f06d3183a3a72233e9a76eb0dd0b976
goygoybords <kevinseankho@yahoo.com>
2015-12-28 10:44:03 +0800

    updated footer and style radio display block

--- wp-content/themes/twentyfifteen/footer.php
+++ wp-content/themes/twentyfifteen/footer.php
@@ -79,7 +79,7 @@
 			</div>
 		</footer>
 
-		<?php //wp_footer(); ?>
+		<?php wp_footer(); ?>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script type = "text/javascript">
 			var data = '<li class="bmg-l-nav__col">' ;
